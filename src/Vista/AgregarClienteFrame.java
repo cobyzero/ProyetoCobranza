9b436e7a893fbdb9ce68f8c797c1e0a11764a6ab
Pooluser <Jeanpoolsamchez@gmail.com>
2024-07-01 23:37:46 -0500

    lleneelvisibledeclientesagregar

--- src/Vista/AgregarClienteFrame.java
+++ src/Vista/AgregarClienteFrame.java
@@ -185,16 +185,7 @@ public class AgregarClienteFrame extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
-        );
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
